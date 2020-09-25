71dc300d64a3e1a4b1b0177b841115e08ff1dd2b
Technote <technote.space@gmail.com>
2020-09-26 00:15:21 +0900

    feat: use log helper

--- __tests__/process.test.ts
+++ __tests__/process.test.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-magic-numbers */
 import nock from 'nock';
 import path from 'path';
-import {Logger} from '@technote-space/github-action-helper';
+import {Logger} from '@technote-space/github-action-log-helper';
 import {disableNetConnect, getApiFixture, getContext, getOctokit} from '@technote-space/github-action-test-helper';
 import {execute} from '../src/process';
 
--- __tests__/utils/github.test.ts
+++ __tests__/utils/github.test.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-magic-numbers */
 import nock from 'nock';
 import path from 'path';
-import {Logger} from '@technote-space/github-action-helper';
+import {Logger} from '@technote-space/github-action-log-helper';
 import {disableNetConnect, getApiFixture, getContext, spyOnStdout, stdoutCalledWith, getOctokit} from '@technote-space/github-action-test-helper';
 import {addAssignees} from '../../src/utils/github';
 
